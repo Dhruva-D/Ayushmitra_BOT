a0db2350c8f70d38b5226cc6635073f970209ea1
Dhruva D <153127408+Dhruva-D@users.noreply.github.com>
2024-12-04 14:27:22 +0530

    Update qachat.py

--- qachat.py
+++ qachat.py
@@ -16,7 +16,7 @@ def get_gemini_response(question):
 # Initialize our Streamlit app
 st.set_page_config(page_title="Q&A Demo")
 
-st.header("FarmNexus - Agricultural Bot")
+st.header("Ayushmitra-BOTt")
 
 # Initialize session state for chat history if it doesn't exist
 if 'chat_history' not in st.session_state:
